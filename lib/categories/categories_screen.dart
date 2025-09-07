dfee3a33d15ad88d3a2d6263b264812fac456a12
Hamza Hrnjic <hamzahrnjic887@gmail.com>
2025-09-07 09:39:45 +0200

    Created a GridView in the main screen of the app, CategoriesScreen, and displayed dummy data for demo purpose

---
+++ lib/categories/categories_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pick your category'),
+      ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+        children: [
+          Text("one", style: TextStyle(color: Colors.white)),
+          Text("one", style: TextStyle(color: Colors.white)),
+          Text("one", style: TextStyle(color: Colors.white)),
+          Text("one", style: TextStyle(color: Colors.white)),
+          Text("one", style: TextStyle(color: Colors.white)),
+          Text("one", style: TextStyle(color: Colors.white)),
+          Text("one", style: TextStyle(color: Colors.white)),
+          Text("one", style: TextStyle(color: Colors.white)),
+        ],
+      ),
+    );
+  }
+}
