331a79865dc560985f59329b50a493714c741220
Hamza Hrnjic <hamzahrnjic887@gmail.com>
2026-05-23 11:55:39 +0200

    Update categories screen with passing actual data to a meals screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/models/category.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/categories_grid_widget.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
-  void _selectCategory(BuildContext context) {
-    Navigator.push(context, MaterialPageRoute(builder: (ctx) => MealsScreen(title: "Some title", meals: [])));
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals.where((meal) => meal.categories.contains(category.id)).toList();
+    Navigator.push(context, MaterialPageRoute(builder: (ctx) => MealsScreen(title: "Some title", meals: filteredMeals)));
   }
 
   @override
@@ -26,7 +28,7 @@ class CategoriesScreen extends StatelessWidget {
         children: [
           for (final category in availableCategories)
             CategoriesGridWidget(category: category, onSelectCategory: () {
-              _selectCategory(context);
+              _selectCategory(context, category);
             })
         ],
       ),
