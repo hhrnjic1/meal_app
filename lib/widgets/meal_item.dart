381a8f6f54f551e84765e7f3da120db0b6c90ba3
Hamza Hrnjic <hamzahrnjic887@gmail.com>
2026-07-18 22:21:43 +0200

    Add meals details screen

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/screens/meal_details.dart';
 import 'package:meals_app/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
@@ -16,7 +17,9 @@ class MealItem extends StatelessWidget {
       clipBehavior: Clip.hardEdge,
       elevation: 2,
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          Navigator.push(context, MaterialPageRoute(builder: (ctx) => MealDetails(meal: meal)));
+        },
         child: Stack(
           children: [
             FadeInImage(
