8cb6ef2f20d48ea39173171d741ef7436fd36414
Hamza Hrnjic <hamzahrnjic887@gmail.com>
2026-07-29 20:21:38 +0200

    Add the Drawer and set a list items in it

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/screens/categories.dart';
 import 'package:meals_app/screens/meals.dart';
+import 'package:meals_app/widgets/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
@@ -41,6 +42,14 @@ class _TabsScreenState extends State<TabsScreen> {
     });
   }
 
+  void _setScreen(String identifier){
+    if(identifier == "Filters"){
+      
+    } else {
+      Navigator.of(context).pop();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     Widget activePage = CategoriesScreen(
@@ -65,6 +74,7 @@ class _TabsScreenState extends State<TabsScreen> {
       appBar: AppBar(
         title: Text(activePageTitle),
       ),
+      drawer: MainDrawer(onSelectScreen: _setScreen,),
       body: activePage,
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedPageIndex,
---
+++ lib/widgets/main_drawer.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/screens/meals.dart';
+
+class MainDrawer extends StatelessWidget {
+  const MainDrawer({super.key, required this.onSelectScreen});
+
+  final void Function(String) onSelectScreen;
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          DrawerHeader(
+            padding: EdgeInsetsGeometry.all(20),
+            decoration: BoxDecoration(
+                gradient: LinearGradient(colors: [
+              Theme.of(context).colorScheme.primaryContainer,
+              Theme.of(context)
+                  .colorScheme
+                  .primaryContainer
+                  .withValues(alpha: 0.8),
+            ])),
+            child: Row(
+              children: [
+                Icon(Icons.fastfood),
+                SizedBox(
+                  width: 18,
+                ),
+                Text(
+                  'Cooking up!',
+                  style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontSize: 28),
+                )
+              ],
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.restaurant),
+            title: Text(
+              'Meals',
+              style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                  color: Theme.of(context).colorScheme.onSurface, fontSize: 20),
+            ),
+            onTap: () => {onSelectScreen('Meals')},
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text(
+              'Filters',
+              style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                  color: Theme.of(context).colorScheme.onSurface, fontSize: 20),
+            ),
+            onTap: () => {onSelectScreen('Filters')},
+          )
+        ],
+      ),
+    );
+  }
+}
