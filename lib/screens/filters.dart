807e3b23275f50c618191612dc4dd18acf5a6b8e
Hamza Hrnjic <hamzahrnjic887@gmail.com>
2026-08-01 22:42:58 +0200

    Change filter screens local states to be handled trough provider

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -3,87 +3,55 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:meals_app/providers/filters_provider.dart';
 import 'package:meals_app/widgets/filter_widget.dart';
 
-class FiltersScreen extends ConsumerStatefulWidget {
+class FiltersScreen extends ConsumerWidget {
   const FiltersScreen({super.key});
 
   @override
-  ConsumerState<FiltersScreen> createState() => _FiltersScreenState();
-}
-
-class _FiltersScreenState extends ConsumerState<FiltersScreen> {
-  var _glutenFreeSwitchValue = false;
-  var _lactoseFreeSwitchValue = false;
-  var _vegetarianSwitchValue = false;
-  var _veganSwitchValue = false;
-
-  @override
-  void initState() {
-    super.initState();
-    final appliedFilters = ref.read(filtersProvider);
-    _glutenFreeSwitchValue = appliedFilters[Filter.glutenFree]!;
-    _lactoseFreeSwitchValue = appliedFilters[Filter.lactoseFree]!;
-    _vegetarianSwitchValue = appliedFilters[Filter.vegetarian]!;
-    _veganSwitchValue = appliedFilters[Filter.vegan]!;
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final filtersMap = ref.watch(filtersProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Your Filters"),
       ),
-      body: PopScope(
-        canPop: false,
-        onPopInvokedWithResult: (didPop, result) {
-          if (didPop) return;
-          ref.read(filtersProvider.notifier).setFilters({
-            Filter.glutenFree: _glutenFreeSwitchValue,
-            Filter.lactoseFree: _lactoseFreeSwitchValue,
-            Filter.vegetarian: _vegetarianSwitchValue,
-            Filter.vegan: _veganSwitchValue,
-          });
-          Navigator.of(context).pop();
-        },
-        child: Column(
-          children: [
-            FilterWidget(
-                title: 'Gluten Free',
-                subtitle: 'Include only gluten free meals',
-                value: _glutenFreeSwitchValue,
-                onChanged: (newValue) => {
-                      setState(() {
-                        _glutenFreeSwitchValue = newValue;
-                      })
-                    }),
-            FilterWidget(
-                title: 'Lactose Free',
-                subtitle: 'Include only lactose free meals',
-                value: _lactoseFreeSwitchValue,
-                onChanged: (newValue) => {
-                      setState(() {
-                        _lactoseFreeSwitchValue = newValue;
-                      })
-                    }),
-            FilterWidget(
-                title: 'Vegetarian',
-                subtitle: 'Include only vegetarian meals',
-                value: _vegetarianSwitchValue,
-                onChanged: (newValue) => {
-                      setState(() {
-                        _vegetarianSwitchValue = newValue;
-                      })
-                    }),
-            FilterWidget(
-                title: 'Vegan Free',
-                subtitle: 'Include only vegetarian meals',
-                value: _veganSwitchValue,
-                onChanged: (newValue) => {
-                      setState(() {
-                        _veganSwitchValue = newValue;
-                      })
-                    }),
-          ],
-        ),
+      body: Column(
+        children: [
+          FilterWidget(
+              title: 'Gluten Free',
+              subtitle: 'Include only gluten free meals',
+              value: filtersMap[Filter.glutenFree]!,
+              onChanged: (newValue) => {
+                    ref
+                        .watch(filtersProvider.notifier)
+                        .setFilter(Filter.glutenFree, newValue)
+                  }),
+          FilterWidget(
+              title: 'Lactose Free',
+              subtitle: 'Include only lactose free meals',
+              value: filtersMap[Filter.lactoseFree]!,
+              onChanged: (newValue) => {
+                    ref
+                        .watch(filtersProvider.notifier)
+                        .setFilter(Filter.lactoseFree, newValue)
+                  }),
+          FilterWidget(
+              title: 'Vegetarian',
+              subtitle: 'Include only vegetarian meals',
+              value: filtersMap[Filter.vegetarian]!,
+              onChanged: (newValue) => {
+                    ref
+                        .watch(filtersProvider.notifier)
+                        .setFilter(Filter.vegetarian, newValue)
+                  }),
+          FilterWidget(
+              title: 'Vegan Free',
+              subtitle: 'Include only vegetarian meals',
+              value: filtersMap[Filter.vegan]!,
+              onChanged: (newValue) => {
+                    ref
+                        .watch(filtersProvider.notifier)
+                        .setFilter(Filter.vegan, newValue)
+                  }),
+        ],
       ),
     );
   }
